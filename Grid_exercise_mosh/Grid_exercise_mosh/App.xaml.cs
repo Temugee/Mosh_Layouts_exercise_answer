5f98959ce60a8e3dc1980bea409d82bac4467deb
Temuge <temuge.erdene@gmail.com>
2024-03-25 14:15:41 +0800

    Add project files.

---
+++ Grid_exercise_mosh/Grid_exercise_mosh/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Grid_exercise_mosh
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new RelativeLayoutExercise();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
